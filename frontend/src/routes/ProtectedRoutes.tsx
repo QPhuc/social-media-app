1597c7836abadf3fe56fe4f1319e897cc6f0ff9d
Phuc Tran <tqp1610@gmail.com>
2025-12-11 18:21:02 +0700

    Update ProtectedRoutes

--- frontend/src/routes/ProtectedRoutes.tsx
+++ frontend/src/routes/ProtectedRoutes.tsx
@@ -1,8 +1,9 @@
-import { useAuth } from "@/context/AuthContext"
 import { Outlet } from "react-router-dom"
+import { useAuth } from "@/context/AuthContext"
 
 export default function ProtectedRoutes() {
 	const { user } = useAuth()
+	console.log(user)
 
 	// if (!user) {
 	// 	return <Navigate replace to="/login" />
