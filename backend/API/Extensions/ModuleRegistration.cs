18d141eb9fcd81a853b56d84bfaa4cdc54d66602
Phuc Tran <tqp1610@gmail.com>
2025-12-13 22:16:37 +0700

    Remove ignored files from repository

---
+++ backend/API/Extensions/ModuleRegistration.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace backend.API;
+
+public static class ModuleRegistration
+{
+    public static void AddModules(this IServiceCollection services)
+    {
+        // Register application modules here
+        
+    }
+}
