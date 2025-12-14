19d87fe5d3a4a601c2fd060cc18262d8334483c3
Phuc Tran <tqp1610@gmail.com>
2025-12-14 08:54:24 +0700

    Add Hashtag Entity

---
+++ backend/Domain/Entities/Hashtag.cs
@@ -0,0 +1,13 @@
+using backend.Domain.Common;
+
+namespace backend.Domain.Entities;
+
+public class Hashtag : BaseEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty; // #travel, #food, ...
+    public int Count { get; set; }
+
+    public DateTime DateCreated { get; set; }
+    public DateTime DateUpdated { get; set; }
+}
