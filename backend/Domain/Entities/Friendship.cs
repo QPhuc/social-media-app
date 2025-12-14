ca1db9f064787e0d84be584c20c26f241876d692
Phuc Tran <tqp1610@gmail.com>
2025-12-14 08:54:13 +0700

    Add Friendship Entity

---
+++ backend/Domain/Entities/Friendship.cs
@@ -0,0 +1,13 @@
+using backend.Domain.Common;
+
+namespace backend.Domain.Entities;
+
+public class Friendship : BaseEntity
+{
+    public int Id { get; set; }
+    public int SenderId { get; set; }
+    public int ReceiverId { get; set; }
+
+    public virtual User Sender { get; set; } = new User();
+    public virtual User Receiver { get; set; } = new User();
+}
