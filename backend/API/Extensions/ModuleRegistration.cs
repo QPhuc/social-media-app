35a67ca3474f2ba41a2dfa4a7ddd47e47daf6866
Phuc Tran <tqp1610@gmail.com>
2025-12-14 21:45:38 +0700

    Update ModuleRegistration.cs file

--- backend/API/Extensions/ModuleRegistration.cs
+++ backend/API/Extensions/ModuleRegistration.cs
@@ -1,12 +1,26 @@
 using System;
+using backend.Application.Common.Interfaces;
+using backend.Infrastructure.Persistence;
+using backend.Infrastructure.Persistence.DbContext;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend.API;
 
 public static class ModuleRegistration
 {
-    public static void AddModules(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        // Register application modules here
-        
+        // DbContext
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        // Unit Of Work
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        // Services
+
+        // Repositories
+
+        return services;
     }
 }
