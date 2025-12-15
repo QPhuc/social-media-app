6311ec012846e5b758b8e958f4e4b440198f9c21
Phuc Tran <tqp1610@gmail.com>
2025-12-15 21:56:18 +0700

    Add JwtBearer

--- backend/API/Program.cs
+++ backend/API/Program.cs
@@ -1,4 +1,7 @@
+using System.Text;
 using backend.API;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace backend;
 
@@ -16,6 +19,30 @@ public class Program
         builder.Services.AddSwaggerGen();           // Registers Swagger generator
 
         builder.Services.AddInfrastructure(builder.Configuration);
+
+        builder.Services
+            .AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)
+                    )
+                };
+            });
+        
+        builder.Services.AddAuthorization();
 
         var app = builder.Build();
 
